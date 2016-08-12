f4639e1a41592c14db630f2fe99f208048c1c288
masuyo <fusewent@gmail.com>
2016-08-12 16:27:18 +0200

    Linkek

---
+++ HostApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using NHLService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(NHLStatService)))
+            {
+                host.Open();
+                Console.WriteLine("RUNNING: {0}", host.BaseAddresses[0].AbsoluteUri);
+                Console.ReadLine();
+            }
+        }
+    }
+}
